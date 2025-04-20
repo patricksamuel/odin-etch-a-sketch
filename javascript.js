cce9f91c76ff91772c5aacba55b6bf1e54aa90db
Patrick Samuel <patrick-samuel@live.com>
2025-04-20 10:53:44 +0200

    16x16 grid squares done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello");
+
+const containerVar = document.querySelector(".container");
+
+for (let i=0; i<16 ; i++) {
+    const rowAdd = document.createElement("div");
+    rowAdd.classList.add("rowContainer");
+    for (let j=0;j<16;j++) {
+        const divAdd = document.createElement("div");
+        divAdd.classList.add("gridMember");
+        rowAdd.appendChild(divAdd);
+
+    }
+    containerVar.appendChild(rowAdd);
+
+}
