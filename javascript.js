aa04a36c243f7804d6769e4eb570589aee39455d
Patrick Samuel <patrick-samuel@live.com>
2025-04-21 03:08:10 +0200

    Button to reset the grid width done. Challenge done.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,60 @@
 console.log("Hello");
 
+
+const genGridVar = document.querySelector("#generateGrid");
+const superContainerVar = document.querySelector(".superContainer");
 const containerVar = document.querySelector(".container");
 
-for (let i=0; i<16 ; i++) {
-    const rowAdd = document.createElement("div");
-    rowAdd.classList.add("rowContainer");
-    for (let j=0;j<16;j++) {
-        const divAdd = document.createElement("div");
-        divAdd.classList.add("gridMember");
-        rowAdd.appendChild(divAdd);
-        divAdd.addEventListener("mouseenter", (event) =>{
-            console.log("hover");
-            event.target.style.background = "purple";
+function genGridFun(tileNum) {
+    containerVar.textContent = "";
 
-            // reset the color after a short delay
-            setTimeout(() => {
-            event.target.style.background = "";
-            }, 500);
-        },false);
+    for (let i=0; i<tileNum ; i++) {
+        const rowAdd = document.createElement("div");
+        rowAdd.classList.add("rowContainer");
+        for (let j=0;j<tileNum;j++) {
+            const divAdd = document.createElement("div");
+            divAdd.classList.add("gridMember");
+            rowAdd.appendChild(divAdd);
+            divAdd.addEventListener("mouseenter", (event) =>{
+                console.log("hover");
+                event.target.style.background = "purple";
+    
+                // reset the color after a short delay
+                setTimeout(() => {
+                event.target.style.background = "";
+                }, 500);
+            },false);
+        }
+        containerVar.appendChild(rowAdd);
     }
-    containerVar.appendChild(rowAdd);
 }
+
+genGridVar.addEventListener("click", () => {
+    let tileNumInput = prompt("Enter the grid width");
+    while (tileNumInput > 100) {
+        tileNumInput = prompt("Enter the grid width");
+    }
+
+    alert("correct");
+    genGridFun(tileNumInput);
+})
+
+// for (let i=0; i<16 ; i++) {
+//     const rowAdd = document.createElement("div");
+//     rowAdd.classList.add("rowContainer");
+//     for (let j=0;j<16;j++) {
+//         const divAdd = document.createElement("div");
+//         divAdd.classList.add("gridMember");
+//         rowAdd.appendChild(divAdd);
+//         divAdd.addEventListener("mouseenter", (event) =>{
+//             console.log("hover");
+//             event.target.style.background = "purple";
+
+//             // reset the color after a short delay
+//             setTimeout(() => {
+//             event.target.style.background = "";
+//             }, 500);
+//         },false);
+//     }
+//     containerVar.appendChild(rowAdd);
+// }
